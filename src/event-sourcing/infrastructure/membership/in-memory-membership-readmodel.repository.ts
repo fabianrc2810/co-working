4b1d0c323fd5fe65d87c1658161fe6f3434fbf4d
freyes <fabianhr.fr@gmail.com>
2025-03-16 05:22:08 -0500

    fix: Access to the membership instance directly.

--- src/event-sourcing/infrastructure/membership/in-memory-membership-readmodel.repository.ts
+++ src/event-sourcing/infrastructure/membership/in-memory-membership-readmodel.repository.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-
 import { Membership } from 'src/event-sourcing/domain/membership/membership.entity';
 import { MembershipReadRepository } from 'src/event-sourcing/domain/membership/membership.repository';
 
@@ -19,7 +15,7 @@ export class InMemoryMembershipReadRepository
   }
 
   async updateReadModel(membership: Membership): Promise<void> {
-    const userId = (membership as any).membershipState.userId.value;
+    const userId = membership.membershipState.userId.value;
     this.memberships.set(userId, membership);
 
     return Promise.resolve();
